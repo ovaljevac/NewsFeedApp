12659e872e95b207bf91b4eda5dc4739a4fec462
ovaljevac1 <ovaljevac1@etf.unsa.ba>
2025-04-19 20:29:26 +0200

    FilterScreen Dodan datepicker, zabranjene rijeci filter i back button

--- app/src/main/java/etf/ri/rma/newsfeedapp/screen/FilterScreen.kt
+++ app/src/main/java/etf/ri/rma/newsfeedapp/screen/FilterScreen.kt
@@ -1,15 +1,19 @@
 package etf.ri.rma.newsfeedapp.screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,14 +25,17 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
@@ -46,7 +53,6 @@ import java.util.Locale
 @Composable
 fun FilterScreen(onBack: () -> Unit){
     var selected by remember { mutableStateOf("Sve") }
-    var aa by remember {mutableStateOf(false)}
     val categories = listOf(
         Categories("Sve", "filter_chip_all"),
         Categories("Politika", "filter_chip_pol"),
@@ -55,13 +61,13 @@ fun FilterScreen(onBack: () -> Unit){
         Categories("Crna hronika", "filter_chip_none"),
     )
     var selectedDate by remember { mutableStateOf<Pair<Long?, Long?>?>(null) }
-    val state = rememberDateRangePickerState()
-    val dateFormat = SimpleDateFormat("DD-MM-YYYY")
     var showModal by remember { mutableStateOf(false) }
-
+    val unwantedList = remember { mutableStateListOf<String>() }
+    var text by remember { mutableStateOf("")}
+    var backButton by remember { mutableStateOf(false) }
     Column(
-        modifier = Modifier.
-            fillMaxHeight()
+        modifier = Modifier
+            .fillMaxHeight()
     ) {
         Text(
                 text = "KATEGORIJE: ",
@@ -71,7 +77,7 @@ fun FilterScreen(onBack: () -> Unit){
         FlowRow(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(3.dp)
+                .padding(vertical = 3.dp, horizontal = 5.dp)
                 .border(2.dp, Color.Black, RoundedCornerShape(6.dp))
         ) {
             categories.forEach { category ->
@@ -102,13 +108,14 @@ fun FilterScreen(onBack: () -> Unit){
 
         Button(
             colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFF3A3A3A), // background color
-                contentColor = Color.White // color of text and icons
+                containerColor = Color(0xFF3A3A3A),
+                contentColor = Color.White
             ),
             modifier = Modifier
-                .padding(3.dp)
+                .padding(vertical = 3.dp, horizontal = 5.dp)
                 .border(2.dp, Color.Black, RoundedCornerShape(6.dp))
-                .testTag("filter_daterange_button"),
+                .testTag("filter_daterange_button")
+                .padding(horizontal = 3.dp),
             onClick = {
                 showModal = true
             }
@@ -125,12 +132,20 @@ fun FilterScreen(onBack: () -> Unit){
             }
         }
         Text(
-            text = "IZABRANI PERIOD: \n$formatted",
+            text = "IZABRANI PERIOD: ",
+            fontWeight = Bold,
+            modifier = Modifier
+                .padding(3.dp)
+        )
+        Text(
+            text = "$formatted",
             fontWeight = Bold,
             modifier = Modifier
                 .padding(3.dp)
                 .testTag("filter_daterange_display")
-
+                .align(alignment = Alignment.CenterHorizontally)
+                .border(2.dp, Color.Black, RoundedCornerShape(6.dp))
+                .padding(horizontal = 6.dp)
         )
         if(showModal){
             DateRangePickerModal(
@@ -140,6 +155,68 @@ fun FilterScreen(onBack: () -> Unit){
                 },
                 onDismiss = { showModal = false }
             )
+        }
+        Text(
+            text = "UNESITE ZABRANJENE RIJEČI: ",
+            fontWeight = Bold,
+            modifier = Modifier
+                .padding(3.dp)
+
+        )
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            TextField(
+                value = text,
+                onValueChange = { text = it },
+                modifier = Modifier
+                    .testTag("filter_unwanted_input")
+                    .padding(vertical = 3.dp, horizontal = 5.dp)
+                    .border(2.dp, Color.Black, RoundedCornerShape(6.dp))
+                    .height(56.dp)
+                    .weight(1f)
+            )
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF3A3A3A),
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(6.dp),
+                modifier = Modifier
+                    .padding(vertical = 3.dp, horizontal = 5.dp)
+                    .height(53.dp),
+                onClick = {
+                    if(text.isNotBlank()){
+                    unwantedList.add(text)
+                    text = ""}
+                }
+            ) {
+                Text("Dodaj")
+            }
+        }
+        UnwantedWordsList(unwantedList)
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            verticalAlignment = Alignment.Bottom,
+             modifier = Modifier.padding(WindowInsets.navigationBars.asPaddingValues())
+
+        ){
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF3A3A3A),
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(6.dp),
+                modifier = Modifier
+                    .padding(vertical = 3.dp, horizontal = 5.dp)
+                    .height(53.dp),
+                onClick = {
+                    backButton = !backButton
+                    onBack()
+                }
+            ) {
+                Text("Nazad")
+            }
         }
     }
     }
---
+++ app/src/main/java/etf/ri/rma/newsfeedapp/screen/UnwantedWordsList.kt
@@ -0,0 +1,47 @@
+package etf.ri.rma.newsfeedapp.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun UnwantedWordsList (unwantedWordsList : List<String>) {
+    Box (
+        modifier = Modifier
+            .padding(horizontal = 3.dp, vertical = 5.dp)
+            .border(2.dp, Color.Black, RoundedCornerShape(6.dp))
+            .fillMaxWidth()
+            .background(color = Color.White)
+    ){
+        LazyColumn {
+            items(unwantedWordsList) { word ->
+                Column() {
+                    Text(
+                        text = word,
+                        modifier = Modifier
+                            .padding(horizontal = 3.dp)
+                    )
+                    Divider(
+                        thickness = 1.dp,
+                        modifier = Modifier
+                            .padding(horizontal = 3.dp, vertical = 5.dp)
+                    )
+                }
+            }
+        }
+    }
+}
