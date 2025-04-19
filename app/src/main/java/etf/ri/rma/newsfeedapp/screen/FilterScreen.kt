86075b07d5efdeb39b7575f163d2516a1f3f491d
ovaljevac1 <ovaljevac1@etf.unsa.ba>
2025-04-19 17:45:41 +0200

    FilterScreen Beta Version

--- app/src/main/java/etf/ri/rma/newsfeedapp/screen/FilterScreen.kt
+++ app/src/main/java/etf/ri/rma/newsfeedapp/screen/FilterScreen.kt
@@ -1,21 +1,24 @@
 package etf.ri.rma.newsfeedapp.screen
-
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Button
-import androidx.compose.material3.DateRangePicker
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilterChip
-import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDateRangePickerState
@@ -24,19 +27,26 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.unit.dp
+import etf.ri.rma.newsfeedapp.customcomposables.DateRangePickerModal
+import etf.ri.rma.newsfeedapp.customcomposables.FilterChipCustom
 import etf.ri.rma.newsfeedapp.model.Categories
 import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
 @Composable
-fun FilterScreen(){
+fun FilterScreen(onBack: () -> Unit){
     var selected by remember { mutableStateOf("Sve") }
-    var dateRangePicker by remember {mutableStateOf(false)}
+    var aa by remember {mutableStateOf(false)}
     val categories = listOf(
         Categories("Sve", "filter_chip_all"),
         Categories("Politika", "filter_chip_pol"),
@@ -44,58 +54,92 @@ fun FilterScreen(){
         Categories("Nauka/tehnologija", "filter_chip_sci"),
         Categories("Crna hronika", "filter_chip_none"),
     )
+    var selectedDate by remember { mutableStateOf<Pair<Long?, Long?>?>(null) }
     val state = rememberDateRangePickerState()
     val dateFormat = SimpleDateFormat("DD-MM-YYYY")
-    FlowRow(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(3.dp)
+    var showModal by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier.
+            fillMaxHeight()
     ) {
-        categories.forEach { category ->
-            FilterChip(
-                modifier = Modifier
-                    .padding(3.dp)
-                    .testTag(category.tag)
-                    .height(50.dp)
-                    .width(115.dp),
-                onClick = { selected = category.cat },
-                label = {
-                    Text(category.cat)
-                },
-                selected = selected == category.cat,
-                leadingIcon = if (selected == category.cat) {
-                    {
-                        Icon(
-                            imageVector = Icons.Filled.Done,
-                            contentDescription = "Done icon",
-                            modifier = Modifier.size(FilterChipDefaults.IconSize)
-                        )
-                    }
-
-                } else {
-                    null
-                },colors = FilterChipDefaults.filterChipColors(
-                    containerColor = Color(0xFF3A3A3A),
-                    selectedContainerColor = Color(0xFF6C4F3D),
-                    labelColor = Color.White,
-                    selectedLabelColor = Color.White,
-                    iconColor = Color.White,
-                    selectedLeadingIconColor = Color.White
-                )
+        Text(
+                text = "KATEGORIJE: ",
+                fontWeight = Bold,
+                modifier = Modifier.padding(3.dp)
             )
-        }
-        Button(
-            onClick = {dateRangePicker = true},
+        FlowRow(
             modifier = Modifier
-                .testTag("filter_daterange_display")
+                .fillMaxWidth()
+                .padding(3.dp)
+                .border(2.dp, Color.Black, RoundedCornerShape(6.dp))
         ) {
-            Text("Izaberite opseg datuma")
+            categories.forEach { category ->
+                FilterChipCustom(
+                    category = category,
+                    selected = selected,
+                    onSelected = { selected = it },
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(110.dp)
+                        .padding(1.dp)
+                )
+            }
         }
-        if(dateRangePicker) {
-            DateRangePicker(
-                state = state
+        Text(
+            text = "DATUM: ",
+            fontWeight = Bold,
+            modifier = Modifier.padding(3.dp)
+        )
+        val formatter = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+        val formatted = selectedDate?.let { (start, end) ->
+            if (start != null && end != null) {
+                "${formatter.format(Date(start))};${formatter.format(Date(end))}"
+            } else {
+                "Nije izabran period"
+            }
+        } ?: "Nije izabran period"
 
+        Button(
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF3A3A3A), // background color
+                contentColor = Color.White // color of text and icons
+            ),
+            modifier = Modifier
+                .padding(3.dp)
+                .border(2.dp, Color.Black, RoundedCornerShape(6.dp))
+                .testTag("filter_daterange_button"),
+            onClick = {
+                showModal = true
+            }
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = null)
+                Spacer(modifier = Modifier.weight(1f))
+                Text("Izaberite opseg datuma")
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
+        Text(
+            text = "IZABRANI PERIOD: \n$formatted",
+            fontWeight = Bold,
+            modifier = Modifier
+                .padding(3.dp)
+                .testTag("filter_daterange_display")
+
+        )
+        if(showModal){
+            DateRangePickerModal(
+                onDateRangeSelected = {
+                    selectedDate = it
+                    showModal = false
+                },
+                onDismiss = { showModal = false }
             )
         }
-        }
+    }
     }
--- app/src/main/java/etf/ri/rma/newsfeedapp/screen/NewsFeedScreen.kt
+++ app/src/main/java/etf/ri/rma/newsfeedapp/screen/NewsFeedScreen.kt
@@ -1,112 +1,72 @@
 package etf.ri.rma.newsfeedapp.screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.FilterChip
-import androidx.compose.material3.FilterChipDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.unit.dp
+import etf.ri.rma.newsfeedapp.customcomposables.FilterChipCustom
 import etf.ri.rma.newsfeedapp.data.NewsData
 import etf.ri.rma.newsfeedapp.model.Categories
 
 @Composable
 fun NewsFeedScreen() {
-        val newsItemsAll = NewsData.getAllNews()
-        var selected by remember { mutableStateOf("Sve") }
-        var filterScreen by remember {mutableStateOf(false)}
-        val newsItemsFilter = if (selected == "Sve") newsItemsAll
-                             else newsItemsAll.filter { it.category == selected }
-        val categories = listOf(
-            Categories("Sve", "filter_chip_all"),
-            Categories("Politika", "filter_chip_pol"),
-            Categories("Sport", "filter_chip_spo"),
-            Categories("Nauka/tehnologija", "filter_chip_sci"),
-            Categories("Crna hronika", "filter_chip_none"),
-            Categories("Više filtera ...", "filter_chip_more")
-        )
-
-    Column(modifier = Modifier
-            .fillMaxSize()
-            .padding(WindowInsets.statusBars.asPaddingValues())
-            .background(color = Color(0xFFD2B48C))) {
-        if (!filterScreen) {
-            LazyRow(
+    val newsItemsAll = NewsData.getAllNews()
+    var selected by remember { mutableStateOf("Sve") }
+    var currentScreen by remember { mutableStateOf("newsFeed") }
+    val newsItemsFilter = if (selected == "Sve") newsItemsAll
+    else newsItemsAll.filter { it.category == selected }
+    val categories = listOf(
+        Categories("Sve", "filter_chip_all"),
+        Categories("Politika", "filter_chip_pol"),
+        Categories("Sport", "filter_chip_spo"),
+        Categories("Nauka/tehnologija", "filter_chip_sci"),
+        Categories("Crna hronika", "filter_chip_none"),
+        Categories("Više filtera ...", "filter_chip_more")
+    )
+    when (currentScreen) {
+        "newsFeed" ->
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .fillMaxSize()
             ) {
-                items(categories) { category ->
-                    FilterChip(
-                        modifier = Modifier
-                            .padding(3.dp)
-                            .testTag(category.tag),
-                        onClick = {
-                            selected = category.cat
-                            if (selected == "Više filtera ...") {
-                                filterScreen = true
-                            }
-                        },
-                        label = {
-                            Text(category.cat)
-                        },
-                        selected = selected == category.cat,
-                        leadingIcon = if (selected == category.cat) {
-                            {
-                                Icon(
-                                    imageVector = Icons.Filled.Done,
-                                    contentDescription = "Done icon",
-                                    modifier = Modifier.size(FilterChipDefaults.IconSize)
-                                )
-                            }
 
-                        } else {
-                            null
-                        }, colors = FilterChipDefaults.filterChipColors(
-                            containerColor = Color(0xFF3A3A3A),
-                            selectedContainerColor = Color(0xFF6C4F3D),
-                            labelColor = Color.White,
-                            selectedLabelColor = Color.White,
-                            iconColor = Color.White,
-                            selectedLeadingIconColor = Color.White
+                LazyRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    items(categories) { category ->
+                        FilterChipCustom(
+                            category = category,
+                            selected = selected,
+                            onSelected = {
+                                if (it == "Više filtera ...") {
+                                    currentScreen = "filter"
+                                } else {
+                                    selected = it
+                                }
+                            },
+                            filterScreen = { currentScreen = "filter" }
                         )
-                    )
+                    }
+                }
+                if (newsItemsFilter.isEmpty() && selected != "Više filtera ...") {
+                    MessageCard("Nema pronađenih vijesti u kategoriji $selected")
+                } else {
+                    key(selected) {
+                        NewsList(newsList = newsItemsFilter)
+                    }
                 }
             }
-        }
-
-            if(filterScreen){
-                FilterScreen()
-            }
-            else if(newsItemsFilter.isEmpty() && selected != "Više filtera ..."){
-                MessageCard("Nema pronađenih vijesti u kategoriji $selected")
-            } else {
-                key(selected) {
-                    NewsList(newsList = newsItemsFilter)
-                }
-            }
-
+        "filter" -> {
+            FilterScreen(onBack = {currentScreen = "newsFeed"})
         }
     }
+}
